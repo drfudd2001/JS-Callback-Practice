a054a453c8f633ec730135936203ad7a2f4b1c7a
drfudd2001 <elmer.figueroagarcia@gmail.com>
2022-01-22 00:46:53 -0600

    Changes with bonus content

--- index.js
+++ index.js
@@ -1,15 +1,57 @@
+// Create the inventory 'div' and place at the bottom of the screen at 100% width
 const inventory = newInventory()
 move(inventory).to(0, 0)
 
-const character = newImage('assets/green-character/static.gif')
-move(character).to(100, 250)
+// Create character that will eventually be movable with largest zIndex
+const character = newImage('./assets/green-character/static.gif')
+character.style.zIndex = 2;
 
+// Change the image used according to the direction of travel
+function handleDirectionChange(direction){
+    if(direction === null){
+        character.src = './assets/green-character/static.gif'
+    }
+    if(direction === 'west'){
+        character.src = './assets/green-character/west.gif'
+    }
+    if(direction === 'north'){
+        character.src = './assets/green-character/north.gif'
+    }
+    if(direction === 'east'){
+        character.src = './assets/green-character/east.gif'
+    }
+    if(direction === 'south'){
+        character.src = './assets/green-character/south.gif'
+    }
+}
 
-move(newImage('assets/tree.png')).to(200, 450)
-move(newImage('assets/pillar.png')).to(350, 250)
-move(newImage('assets/pine-tree.png')).to(450, 350)
-move(newImage('assets/crate.png')).to(150, 350)
-move(newImage('assets/well.png')).to(500, 575)
-move(newItem('assets/sword.png')).to(500, 555)
-move(newItem('assets/shield.png')).to(165, 335)
-move(newItem('assets/staff.png')).to(600, 250)
+// Moves the green character with arrow keys
+move(character).withArrowKeys(100, 250, handleDirectionChange)
+
+// Create an array of objects, containing all information for initial placement
+let imgData = [
+    {id: "tree", leftOffset: 200, bottomOffset: 450, imgPath: './assets/tree.png'},
+    {id: "pillar", leftOffset: 350, bottomOffset: 250, imgPath: './assets/pillar.png'},
+    {id: "pine-tree", leftOffset: 450, bottomOffset: 350, imgPath: './assets/pine-tree.png'},
+    {id: "crate", leftOffset: 150, bottomOffset: 350, imgPath: './assets/crate.png'},
+    {id: "well", leftOffset: 500, bottomOffset: 575, imgPath: './assets/well.png'}
+]
+
+// Use the imgData array to create and place the images
+let lengthImgData = imgData.length
+for (let i = 0; i < lengthImgData; i++) {
+    move(newImage(imgData[i].imgPath)).to(imgData[i].leftOffset, imgData[i].bottomOffset)
+}
+
+// Create an array of objects, containing all information for initial placement
+let itemData = [
+    {id: "sword", leftOffset: 500, bottomOffset: 555, imgPath: './assets/sword.png'},
+    {id: "shield", leftOffset: 165, bottomOffset: 335, imgPath: './assets/shield.png'},
+    {id: "staff", leftOffset: 600, bottomOffset: 250, imgPath: './assets/staff.png'}
+]
+
+// Use the itemData array to create and place inventory items that can be clicked to move to inventory
+let lengthItemData = itemData.length
+for (let i = 0; i < lengthItemData; i++) {
+    move(newItem(itemData[i].imgPath)).to(itemData[i].leftOffset, itemData[i].bottomOffset)
+}
--- move.js
+++ move.js
@@ -6,7 +6,107 @@ function move(element) {
         element.style.bottom = bottom + 'px'
     }
 
+    // Allows for the movement of the green character with the use of the arrow keys
+    function moveWithArrowKeys(left, bottom, callback){
+        let direction = null;
+        let x = left;
+        let y = bottom;
+    
+        element.style.left = x + 'px'
+        element.style.bottom = y + 'px'
+        
+        // By making use of the image array, this allows the green caharacter to walk behind or in front of the images on screen
+        function changeCharacterZindex(){
+            //Tree
+            if(imgData[0].leftOffset - 40 < x && x < imgData[0].leftOffset + 120){
+                if (imgData[0].bottomOffset < y && y < imgData[0].bottomOffset + 165){
+                    character.style.zIndex = 0;
+                }
+            } 
+            //Pillar
+            else if(imgData[1].leftOffset - 40 < x && x < imgData[1].leftOffset + 60){
+                if (imgData[1].bottomOffset < y && y < imgData[1].bottomOffset + 200){
+                    character.style.zIndex = 0;
+                }
+            } 
+            //Pine-tree
+            else if(imgData[2].leftOffset - 40 < x && x < imgData[2].leftOffset + 145){
+                if (imgData[2].bottomOffset < y && y < imgData[2].bottomOffset + 207){
+                    character.style.zIndex = 0;
+                }
+            } 
+            //Crate
+            else if(imgData[3].leftOffset - 40 < x && x < imgData[3].leftOffset + 20){
+                if (imgData[3].bottomOffset < y && y < imgData[3].bottomOffset + 25){
+                    character.style.zIndex = 0;
+                }
+            } 
+            //Well
+            else if(imgData[4].leftOffset - 40 < x && x < imgData[4].leftOffset + 95){
+                if (imgData[4].bottomOffset < y && y < imgData[4].bottomOffset + 100){
+                    character.style.zIndex = 0;
+                }
+            } 
+            //Designates green character as having the largest zIndex
+            else {
+                character.style.zIndex = 2;
+            }
+        }
+
+        setInterval(changeCharacterZindex, 1)
+
+        //Limits the screen area that the green character is allowed to move in
+        function moveCharacter(){ 
+            if(direction === 'west' && x > 0){
+                x-=1
+            }
+            if(direction === 'north' && y < window.innerHeight - 75){
+                y+=1
+            }
+            if(direction === 'east' && x < window.innerWidth - 50){
+                x+=1
+            }
+            if(direction === 'south' && y > 100){
+                y-=1
+            }
+            element.style.left = x + 'px'
+            element.style.bottom = y + 'px'
+        }
+        
+        setInterval(moveCharacter, 1)
+        
+        //Event Listener that detects the use of arrow keys for the movement of the green character
+        document.addEventListener('keydown', function(e){
+            if(e.repeat) return;
+        
+            if(e.key === 'ArrowLeft'){
+                direction = 'west'
+            }
+            if(e.key === 'ArrowUp'){
+                direction = 'north'
+            }
+            if(e.key === 'ArrowRight'){
+                direction = 'east'
+            }
+            if(e.key === 'ArrowDown'){
+                direction = 'south'
+            }
+            if(callback) {
+                callback(direction)
+            }
+        })
+        
+        //Event Listener that detects when user stops pressing arrow keys to stop the movement of the green character
+        document.addEventListener('keyup', function(e){
+            direction = null
+            if(callback){
+                callback(direction)
+            }
+        })
+    }
+
     return {
-        to: moveToCoordinates
+        to: moveToCoordinates,
+        withArrowKeys: moveWithArrowKeys
     }
 }
